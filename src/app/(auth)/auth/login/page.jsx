8d84da8c9aba23824d0d7ec7859a9ccefda4ed71
Mauricio Santelices <m.santelicesa@gmail.com>
2024-02-20 16:40:07 -0300

    qa

--- src/app/(auth)/auth/login/page.jsx
+++ src/app/(auth)/auth/login/page.jsx
@@ -1,4 +1,8 @@
-import { BottomWave, LoginForm, LoginImage, TopWave } from './components';
+// import { BottomWave, LoginForm, LoginImage, TopWave } from './components';
+import BottomWave from './components/BottomWave/BottomWave';
+import LoginForm from './components/LoginForm/LoginForm';
+import LoginImage from './components/LoginImage/LoginImage';
+import TopWave from './components/TopWave/TopWave';
 import styles from './styles/Login.module.css';
 
 function login() {
